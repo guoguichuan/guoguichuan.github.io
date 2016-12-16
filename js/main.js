780a908550d60119084e0745b5eb239fadbf4f5f
GUICHUAN_PC\jim <guoguichuan@yeah.net>
2016-12-17 01:26:50 +0800

    2016.12.17-01

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function top_left_show()
+{
+	document.getElementById('experience_top_left_hover').style.display='block'
+}
+function top_left_hide()
+{
+	document.getElementById('experience_top_left_hover').style.display='none'
+}
+function top_right_show()
+{
+	document.getElementById('experience_top_right_hover').style.display='block'
+}
+function top_right_hide()
+{
+	document.getElementById('experience_top_right_hover').style.display='none'
+}
+function buttom_show()
+{
+	document.getElementById('experience_bottom_hover').style.display='block'
+}
+function buttom_hide()
+{
+	document.getElementById('experience_bottom_hover').style.display='none'
+}
+function hobbes_top_left_show()
+{
+	document.getElementById('hobbies_top_left_hover').style.display='block'
+}
+function hobbes_top_left_hide()
+{
+	document.getElementById('hobbies_top_left_hover').style.display='none'
+}
+function hobbes_top_right_show()
+{
+	document.getElementById('hobbies_top_right_hover').style.display='block'
+}
+function hobbes_top_right_hide()
+{
+	document.getElementById('hobbies_top_right_hover').style.display='none'
+}
+function hobbes_buttom_show()
+{
+	document.getElementById('hobbies_bottom_hover').style.display='block'
+}
+function hobbes_buttom_hide()
+{
+	document.getElementById('hobbies_bottom_hover').style.display='none'
+}
